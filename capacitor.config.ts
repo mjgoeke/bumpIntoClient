00c6404645bcf1de2fa6d24d700abebc776a6797
Michael Goeke <mjgoeke@yahoo.com>
2025-02-05 00:14:44 -0800

    bun i @capacitor/core, bun i -D @capacitor/cli, bunx cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.diamondhandsllc.bumpinto',
+  appName: 'BumpInto',
+  webDir: 'dist'
+};
+
+export default config;
